729d14f8ad4942359f1cec2c5a0b555660fe7c6a
Thiago Omoto <thi.omoto@gmail.com>
2022-08-17 17:59:27 -0300

    arrumando o lint

--- src/vqv.js
+++ src/vqv.js
@@ -26,10 +26,9 @@ const vqv = (nome, idade) => {
 let frase = `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`
+#VQV!`;
 
 return frase;
-
 };
 
 module.exports = vqv;
