7a46ad09f46f690af2d9eac2361fb51f5db5e47a
Thiago Omoto <thi.omoto@gmail.com>
2022-08-18 14:00:47 -0300

    requisito 9 feito

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -43,13 +43,30 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(getCharacter()).toBe(undefined);
+  });
+  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    expect(getCharacter('Arya')).toHaveProperty('name', 'class', 'phrases');
+  });
+  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    expect(getCharacter('Brienne')).toHaveProperty('name', 'class', 'phrases');
+  });
+  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    expect(getCharacter('Melissandre')).toHaveProperty('name', 'class', 'phrases');
+  });
+  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
     // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
+    expect(getCharacter('ArYa')).toHaveProperty('name', 'class', 'phrases');
+    expect(getCharacter('ARYA')).toHaveProperty('name', 'class', 'phrases');
+  });
+  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    expect(getCharacter('Xablau')).toBe(undefined);
   });
 });
